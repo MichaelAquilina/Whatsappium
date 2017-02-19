6c9dabfd92e391b5ce5175934ba4424f6ba72488
Michael Aquilina <michaelaquilina@gmail.com>
2017-02-19 19:12:55 +0000

    Add keyboard shortcuts for navigating chats

--- whatsappium.js
+++ whatsappium.js
@@ -1,1 +1,55 @@
-document.body.style.border = "5px solid red";
+function dispatchClick (item) {
+  item.dispatchEvent(new window.MouseEvent('mousedown', {
+    view: window,
+    bubbles: true,
+    cancelable: false
+  }));
+}
+
+function makeActive (item) {
+  const chat = item.querySelector('.chat');
+  if (chat) {
+    dispatchClick(chat);
+  }
+}
+
+function isItemActive (item) {
+    const chat = item.querySelector('.chat');
+    return chat && chat.classList.contains('active');
+}
+
+function getChatList () {
+    const chatListElem = document.querySelectorAll('.infinite-list-item');
+    if (chatListElem && chatListElem.length) {
+      return Array.from(chatListElem).sort(function (a, b) {
+        return parseInt(b.style.zIndex, 10) - parseInt(a.style.zIndex, 10);
+      });
+    }
+}
+
+function navigateConverstaion(delta) {
+    const chatList = getChatList();
+
+    let index = -1;
+    for(let i=0; i < chatList.length; i++) {
+      const item = chatList[i]
+      if (isItemActive(item)) {
+          index = i + delta;
+          break;
+      }
+    }
+
+    if (index >= 0 &&  index < chatList.length) {
+      let target = chatList[index];
+      makeActive(target);
+    }
+}
+
+window.onkeyup = function(e){
+    console.log(e.keyCode);
+    if(e.ctrlKey && e.keyCode == 40) {
+      navigateConverstaion(1);
+    } else if (e.ctrlKey && e.keyCode == 38) {
+      navigateConverstaion(-1);
+    }
+}
